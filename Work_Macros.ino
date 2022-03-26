22bb2328dfff23f07191e801ff6ce5a2cb2eaf7b
Isaiah Payton <isaiah20412@outlook.com>
2022-03-26 07:25:11 -0700

    Defined and setup input button pins.

--- Work_Macros.ino
+++ Work_Macros.ino
@@ -1,12 +1,34 @@
+//Libraries
 #include <Adafruit_SSD1306.h>
+
+//Define names for pins
+//Outputs
+#define button1 = 1;
+#define button2 = 2;
+#define button3 = 3;
+#define button4 = 6;
+#define button5 = 7;
+#define button6 = 8;
+#define button7 = 9;
+
+//Arrays for initializing things
+const int inputs[7] = {1, 2, 3, 6, 7, 8, 9};
 
 
 void setup()
 {
-	
+    //Setup serial communication
+	Serial.begin(115200);
+    
+    //Initialize inputs
+    for (int i = 0; i < 7; i++)
+    {
+        pinMode(inputs[i], INPUT);        
+    }
+    
 }
 
 void loop()
 {
-	
+    
 }
--- build/sketch/Work_Macros.ino.cpp
+++ build/sketch/Work_Macros.ino.cpp
@@ -1,20 +1,42 @@
 #include <Arduino.h>
 #line 1 "c:\\Users\\Isaiah\\Documents\\Arduino\\Sketches\\Work_Macros\\Work_Macros.ino"
+//Libraries
 #include <Adafruit_SSD1306.h>
 
+//Define names for pins
+//Outputs
+#define button1 = 1;
+#define button2 = 2;
+#define button3 = 3;
+#define button4 = 6;
+#define button5 = 7;
+#define button6 = 8;
+#define button7 = 9;
 
-#line 4 "c:\\Users\\Isaiah\\Documents\\Arduino\\Sketches\\Work_Macros\\Work_Macros.ino"
+//Arrays for initializing things
+const int inputs[7] = {1, 2, 3, 6, 7, 8, 9};
+
+
+#line 18 "c:\\Users\\Isaiah\\Documents\\Arduino\\Sketches\\Work_Macros\\Work_Macros.ino"
 void setup();
-#line 9 "c:\\Users\\Isaiah\\Documents\\Arduino\\Sketches\\Work_Macros\\Work_Macros.ino"
+#line 31 "c:\\Users\\Isaiah\\Documents\\Arduino\\Sketches\\Work_Macros\\Work_Macros.ino"
 void loop();
-#line 4 "c:\\Users\\Isaiah\\Documents\\Arduino\\Sketches\\Work_Macros\\Work_Macros.ino"
+#line 18 "c:\\Users\\Isaiah\\Documents\\Arduino\\Sketches\\Work_Macros\\Work_Macros.ino"
 void setup()
 {
-	
+    //Setup serial communication
+	Serial.begin(115200);
+    
+    //Initialize inputs
+    for (int i = 0; i < 7; i++)
+    {
+        pinMode(inputs[i], INPUT);        
+    }
+    
 }
 
 void loop()
 {
-	
+    
 }
 
